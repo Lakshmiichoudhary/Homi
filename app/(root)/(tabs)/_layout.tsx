39f94433c3eca9c753df6017e0214636fc8d98d0
Lakshmi B <143175780+Lakshmiichoudhary@users.noreply.github.com>
2026-09-21 20:01:42 +0530

    tabs UI

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -4,22 +4,22 @@ export default function TabLayout() {
   return (
     <NativeTabs>
       <NativeTabs.Trigger name="index">
-        <Icon sf="house.fill" />
+        <Icon sf="house.fill" drawable="ic_home" />
         <Label>Home</Label>
       </NativeTabs.Trigger>
 
       <NativeTabs.Trigger name="search">
-        <Icon sf="magnifyingglass" />
+        <Icon sf="magnifyingglass" drawable="ic_search" />
         <Label>Search</Label>
       </NativeTabs.Trigger>
 
       <NativeTabs.Trigger name="saved">
-        <Icon sf="heart.fill" />
+        <Icon sf="heart.fill" drawable="ic_favorite" />
         <Label>Saved</Label>
       </NativeTabs.Trigger>
 
       <NativeTabs.Trigger name="profile">
-        <Icon sf="person.fill" />
+        <Icon sf="person.fill" drawable="ic_person" />
         <Label>Profile</Label>
       </NativeTabs.Trigger>
     </NativeTabs>
